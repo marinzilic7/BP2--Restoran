c7b49cc85a9588b9f5b3c8ff46f8890eff4837b5
Contineo <marinzilic507@gmail.com>
2023-06-24 15:03:25 +0200

    RestoranApp

--- model/app.py
+++ model/app.py
@@ -4,7 +4,7 @@ from jelo import Jelo
 
 import os 
 
-from flask import Flask,render_template
+from flask import Flask,render_template, request,redirect, url_for
 
 
 
@@ -15,7 +15,20 @@ app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '.
 @app.route("/")
 def index ():
     jela = session.query(Jelo).all()
-    return render_template('home.html', jelo=jela)
+    return render_template('home.html', jela=jela)
+
+@app.route('/dodaj-jelo', methods=['POST'])
+def dodaj_jelo():
+    naziv = request.form.get('naziv')
+    opis = request.form.get('opis')
+    cijena = int(request.form.get('cijena'))
+
+    novo_jelo = Jelo(naziv=naziv, opis=opis, cijena=cijena)
+    session.add(novo_jelo)
+    session.commit()
+
+    
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
